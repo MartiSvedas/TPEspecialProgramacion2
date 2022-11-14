7e0ee87023a26ebebd876d6a92d40feda1833876
MartiSvedas <90530390+MartiSvedas@users.noreply.github.com>
2022-11-14 17:50:04 -0300

    elimino y agrego elemento. Iterable

--- src/ComparadorValor.java
+++ src/ComparadorValor.java
@@ -3,9 +3,12 @@ import java.util.Comparator;
 public class ComparadorValor implements Comparator<Object> {
 
 	@Override
-	public int compare(Object arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compare(Object o1, Object o2) {
+		Integer in1= (Integer) o1;
+		Integer in2= (Integer) o2;
+		
+		return in1.compareTo(in2);
+
 	}
 	
 }
--- src/ListaVinculada.java
+++ src/ListaVinculada.java
@@ -1,31 +1,71 @@
 import java.util.Comparator;
 import java.util.Iterator;
 
-public class ListaVinculada implements Iterator<Object>{
+public class ListaVinculada implements Iterable<Object>{
+	
 	Nodo cabeza;
 	Comparator<Object> orden;
-	private int size;
+	private int size, valor;
 	
 	public ListaVinculada(Comparator <Object> orden) {
 		this.cabeza=null;
-		this.size=0;
+//		this.size=0;
 		this.orden=orden;
+		this.valor=-1;
 	}
 	
-	public void addPrimero(Object obj) {
+	public void setValor(int valor) {
+		if(valor!=0) {
+			this.valor = valor;	
+		}
+	}
+
+	public void addNodo(Object obj) {
 		Nodo nuevo= new Nodo(obj);
-		if(cabeza==null) {
+		if(estaVacia()) {
 			cabeza = nuevo;
 		}else {
-			Nodo temp = cabeza;
-			nuevo.enlazarSiguiente(temp);
-			cabeza=nuevo;
+			Nodo anterior=null;
+			Nodo temp = this.cabeza;
+			while(temp!=null && orden.compare(temp, nuevo)<this.valor) {
+				anterior = cabeza;
+				temp=temp.obtenerSiguiente();
+			}if(temp==null) {
+				anterior.enlazarSiguiente(nuevo);
+			}else {
+				nuevo.enlazarSiguiente(temp);
 		}
-		size++; 
+		}
+//		size++; 
+	}
+	
+	public Object obtener(Integer index) {
+		int contador =0;
+		Nodo temp= cabeza;
+		while(contador<index) {
+			temp= temp.obtenerSiguiente();
+			contador ++;
+		}
+		return temp.obtenerValor();
+	}
+	
+	public void eliminarSegunPosicion(Integer index) {
+		int contador = 0;
+		if(index==0) {
+			cabeza=cabeza.obtenerSiguiente();
+		}else {
+			Nodo temporal = cabeza;
+			while(contador<index-1) {
+				temporal = temporal.obtenerSiguiente();
+				contador ++;
+			}	
+			temporal.enlazarSiguiente(temporal.obtenerSiguiente().obtenerSiguiente());	
+		}
+		
 	}
 	
 	public boolean estaVacia() {
-		return (cabeza==null);
+		return (this.cabeza==null);
 	}
 	
 	public int getSize() {
@@ -37,17 +77,29 @@ public class ListaVinculada implements Iterator<Object>{
 		this.orden = orden;
 	}
 
-	@Override
-	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		return false;
+	public Iterator<Object> iterator(){
+		return new IteradorNodo();
+	}
+	
+	private class IteradorNodo implements Iterator<Object>{
+		private Nodo nodo;
+		
+		public IteradorNodo() {
+			this.nodo= null;
+		}
+		@Override
+		public boolean hasNext() {
+			return nodo.obtenerSiguiente()!=null;
+		}
+		
+		@Override
+		public Nodo next() {
+			return nodo.obtenerSiguiente();
+		}
 	}
 
-	@Override
-	public Nodo next() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
 	
 	
 }
